0e5bcbdbb4c39c5e3f8613b8f9c92e83ed694ebf
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-04-09 18:12:37 +0200

    FIX - repo - FEAT - bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,39 @@
+<script>
+
+    export default{
+        data(){
+            return{
+                title:'a place to open your mind'
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+    <div class="hero">
+        <h1 class="hero__title">
+        {{ title }}
+        </h1>
+    </div>
+    
+</template>
+
+<style>
+
+    .hero{        
+        height: 550px;
+        background-image: url('..\assets\hero.png');
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    .hero__title{
+        text-align:center;
+        font-size: 24px;
+        font-weight: 500;
+        color: rgb(102, 118, 241);
+    }
+    
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,31 @@
+<script>
+
+    export default{
+        data(){
+            return{
+                title: 'La mia prima app con Vite!'
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+    <h1 class="title">
+        {{ title }}
+    </h1>
+
+</template>
+
+<style>
+
+    .title{
+        text-align: center;
+        padding: 10px;
+        font-size: 48px;
+        font-weight: 800;
+        color: rgb(102, 118, 241);
+    }
+    
+</style>
